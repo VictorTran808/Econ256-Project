941355d3d633377cf37ee2b50160fe3d728a92a8
VictorTran808 <victoramostran@gmail.com>
2020-03-17 20:45:16 -1000

    Add data cleaning for Test-dashboard driver code.

--- Test-Dashboard/app.R
+++ Test-Dashboard/app.R
@@ -1,35 +1,64 @@
+#=============================================================================
+#_________________________Import Packages_____________________________________
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(leaflet)
 library(sf)
-Income <- read_csv("Raw-Dataset/Income.csv")
-solar_permits <- read_csv("Raw-Dataset/solar_permits.csv")
-tractsf <- read_sf(dsn = "Raw-Dataset", layer = "tl_2019_15_tract")
+#=============================================================================
+#_________________________Import Dataset______________________________________
+Income <- read_csv("Dataset/Income.csv")
+solar_permits <- read_csv("Dataset/solar_permits.csv")
+tractsf <- read_sf(dsn = "Dataset", layer = "tl_2019_15_tract")
 
 
+#=============================================================================
+#_________________________Cleaning Data_______________________________________
+#Filter Honolulu County area only
 Oahutract <- filter(tractsf, COUNTYFP == "003")
-
+#Rename ID Geography into GEOID so the data can join the shape file data of Oahu
 Rename_Income <- Income %>% rename(GEOID = "ID Geography")
 Rename_Income <- mutate(Rename_Income, GEOID = str_remove(Rename_Income$GEOID, "14000US"))
+#Combine the two data together
 censusInc <-  left_join(Oahutract, Rename_Income, by='GEOID')
+#Remove the Northwestern Hawaii Island from the Honolulu County
 censusInc <- filter(censusInc, NAME != '9812')
+#Filter only data from year 2017 & NA
 censusInc <- filter(censusInc, Year == 2017 | is.na(Year) == TRUE)
+#Remove this census which is the surround body of water around Oahu
 censusInc <- filter(censusInc, NAME != '9900.01')
-
-
-Household_Income <- censusInc$`Household Income by Race`
+#Rename "Household Income by Race" to household_Inc
 censusInc <- censusInc %>% rename(Household_Inc = "Household Income by Race")
+#Lets try to remove unnescessary column
+censusInc <- select(censusInc, -c("Household Income by Race Moe",
+                                       "ID Race",
+                                       "ID Year",
+                                       "Race",
+                                       "FUNCSTAT",
+                                       "MTFCC",
+                                       "Geography",
+                                       "ALAND",
+                                       "AWATER"))
+#Renaming the variables for longitude & latitude
+censusInc <- rename(censusInc, lng = "INTPTLON")
+censusInc <- rename(censusInc, lat = "INTPTLAT")
+#Turn all na values in Household_Inc to zero
+censusInc$Household_Inc[is.na(censusInc$Household_Inc)] = 0
+
+#================================================================================
+#__________________________Import Graphic & Icons Here_____________________________
 #Import Icon
 PV_Icon <- makeIcon(
     iconUrl = "Images/pv_icon.png",
     iconWidth = 30, iconHeight = 30
     
 )
-# PV_lon <- solar_permits$lng
-# PV_lat <- solar_permits$lat
 
+
+#================================================================================
+#_________________________Color & Aesthetic Variables Here_______________________
 # Now redefining pal variable again, but as a colorNumeric function. 
+Household_Income <- censusInc$Household_Inc
 pal <- colorNumeric(palette = "viridis", 
                     domain = Household_Income)
 
@@ -94,7 +123,10 @@ ui <- dashboardPage(title = "Dashboard-test", skin = "green",
                         
                         
                     ),
-                    
+                    #The dashboardBody is where we call function we make in the server code black to be shown on the shiny app
+                    #For example: In tabItem, where tabName = PV, I call the function leafletoutput("map"), where "map" is a variable I made in 
+                    #the server code block called output$map
+                    #Do the same for all the tabItem
                     dashboardBody(
                         tabItems(
                                 tabItem(tabName = "PV",h1("PV"), fluidRow(box(title = "PV location on Oahu",
@@ -129,22 +161,21 @@ ui <- dashboardPage(title = "Dashboard-test", skin = "green",
 #=============================================================================
 #__________________________SERVER FUNCTION_____________________________________
 server <- function(input, output) {
-    
+    #This is a function called 'map', which display the interactive leaflet map for PV location
     output$map<-renderLeaflet({
    
         
         leaflet()%>% 
             addTiles()%>%
-            addMarkers(data = solar_permits[1:500,], lng = ~lng, lat = ~lat, icon = PV_Icon, clusterOptions = markerClusterOptions())
+            addMarkers(data = solar_permits, lng = ~lng, lat = ~lat, icon = PV_Icon, clusterOptions = markerClusterOptions())
         
         
         
               
     })
+    #This is the function called 'map_inc' which display the interactive choropleth map for Income
     output$map_inc <- renderLeaflet({
        
-        #NEw remapping looks goods. Small issues such as the legend value are reverse
-        #Lowest income at the top of the bar and highest at the bottom of the bar. 
         censusInc %>%
             st_transform(crs = "+init=epsg:4326") %>%
             leaflet(width = "100%") %>%
@@ -162,9 +193,51 @@ server <- function(input, output) {
                       opacity = 1) %>%
             addTiles()
     })
-    # output$histogram <- renderPlot({
-    #     hist(faithful$eruptions,breaks = input$bins)
+    
+    #__________________________________________________________________________________________
+    #Lenny & Elenor, pls write your function below when you are ready with your data
+    #Use the form:
+    #
+    # output$name_of_your_variable <- type_of_render_you_are_using ({
+    #
+    #
+    #   Copy & paste the code that does your plotting inside here
+    #   
+    #
     # })
+    #________________________________________________________________________________________
+    
+    #EV code HERE
+    
+    
+    
+    
+    
+    #House Price HERE
+    
+    
+    
+    
+    #Electric Usage HERE
+    
+    
+    
+    
+    
+    #Solar Intensity HERE
+    
+    
+    
+    
+    
+    
+    #_______________________________________________________________________________________
+    # Now the function 'name_of_your_variable' can be call into the dashboardbody code block
+    # An example would be to call the function to where the tabItem of your selected data is 
+    # Ex:
+    # tabItem(tabName = "House_Price",h1("House Price"), name_of_your_variable)
+
+    
     
 }
 #_____________________________________________________________________________
