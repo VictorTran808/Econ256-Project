2843ad9cd72c8ea7035f0619ae569d0b84ef18b9
eleanory808 <eleanory@hawaii.edu>
2020-04-14 15:34:59 -1000

    updated with all PV data points

--- eleczip.R
+++ eleczip.R
@@ -1,20 +1,20 @@
 
-#try
+
+
+
+
 
 library(sf) #simple feature
-dataset <- read_sf(dsn = "masterdata", layer = "masterdata")
-zips <- read_sf(dsn = "Zip_Codes", layer = "Zip_Codes")
-eleczip <- read_csv("eleczip.csv")
 
 #NEW CODE
-electricity <- read_sf(dsn = "tidy_electric", layer = "tidy_electric")
-pvdata <- read_sf(dsn = "masterdata", layer = "masterdata")
+electricity <- read_sf(dsn = "tidy_electric", layer = "tidy_electric") #average electricity usage per zipcode
+pvdata <- read_sf(dsn = "solarzip", layer = "solarzip") #zipcodes of ALL solar projects
 ggplot()+
   geom_sf(data = electricity, aes(fill = electricity$kWh.mo))+
   scale_fill_gradient(low = "yellow", high = "orangered2", na.value = NA)+
   theme_void()+
-  labs(title = "Average Electricity Used per Month According to Zip Code", caption = "Data Source: HECO", fill = "kWh per Month")+
-  geom_sf(data = pvdata, color = "dodgerblue4")
+  labs(title = "Average Electricity Used per Month According to Zip Code", caption = "Data Source: HECO & City and County of Honolulu", fill = "kWh per Month")+
+  geom_sf(data = pvdata, color = "dodgerblue4") #ALL pv points
 
 
 
